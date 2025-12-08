16b33589e4fbebcbe7d47c8a0d0446e8eac30c07
adarsh-mishra07 <viratadarshmishra07837@gmail.com>
2025-12-09 03:01:54 +0530

    File Handling : Login System

---
+++ Day10/Project1.py
@@ -0,0 +1,48 @@
+#Login System using python
+# where the username and password store in the JSON file
+#program read the data from json file at the time of login
+
+import json
+import os
+
+def register():
+    username=input("Create username:")
+    password=input("Create password:")
+
+    data={"username":username,"password":password}   # store in form of json
+
+    with open("users.json","w") as f:
+        json.dump(data,f)
+
+    print("Register sucessful !")
+
+def login():
+    username=input("Enter username:")
+    password=input("Enter the password")
+
+    if os.path.exists("users.json"):
+        with open("users.json","r") as f:
+            data=json.load(f)
+
+        if(data["username"]==username and data["password"]==password):
+            print("Login successfull")
+        else:
+            print("Invallid credentials")
+    else:
+        print("No user found , please register first")
+
+
+
+print("1. Register")
+print("2. Login")
+
+choice=input("choose option:")
+
+if(choice=="1"):
+    register()
+elif choice=="2":
+    login()
+else:
+    print("Invalid choice")
+  
+
