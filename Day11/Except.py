5507ade20e753550edd009e2d7877a220e8f9aee
adarsh-mishra07 <viratadarshmishra07837@gmail.com>
2025-12-21 23:12:14 +0530

    Exception Handling

---
+++ Day11/Except.py
@@ -0,0 +1,60 @@
+#single exception
+"""
+try:
+    num=int(input("Enter a number:"))
+    print(10/num)
+except ZeroDivisionError:
+    print("O cant divide by zero")
+except ValueError:
+    print("number is divide by only integer value")
+
+    
+    """
+
+
+
+#multiple except block
+
+"""
+try:
+    a=int (input("Enter the f number"))
+    b=int (input("enter the s number :"))
+    print(a/b)
+except(ValueError , ZeroDivisionError):
+    print("Error occured")
+else:
+    print("No error")
+
+"""
+
+#finally block always runs
+
+"""
+try:
+    even=int(input("enter the even number"))
+    if(even%2==0):
+       print("even number")
+    else:
+        print("odd number")
+finally:
+    print("I am finally")
+
+"""
+
+"""
+#custom exception 
+
+age=int(input("Enter your age "))
+
+if age<18:
+    raise ValueError("Age must be 18 or above")
+else: 
+    print("Allowed")
+
+"""
+
+try:
+    with open("data.txt", "r") as f:
+        print(f.read())
+except FileNotFoundError:
+    print("File nahi mili")
